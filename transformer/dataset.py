69c31f3cb18839b6bf14900d0a1b17cda65ae867
Lawrence Francis <lawrencedikeu@gmail.com>
2024-06-09 23:10:58 +0200

    Update dataset and model

--- transformer/dataset.py
+++ transformer/dataset.py
@@ -1,4 +1,109 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
+from tokenizer import build_tokenizer
+import time
+
+from datasets import load_dataset
+hf_dataset = load_dataset("Helsinki-NLP/opus-100", "en-fr")
+
+
+class EnFrDataset(Dataset):
+    def __init__(self, split="train", context_len=512) -> None:
+        super().__init__()
+        self.split = split
+        self.en_tokenizer = build_tokenizer("en")
+        self.fr_tokenizer = build_tokenizer("fr")
+        self.pairs = hf_dataset[split]["translation"]
+        self.context_len = context_len
+        
+    def __len__(self):
+        return len(self.pairs)
+
+    def __getitem__(self, index):
+        pair = self.pairs[index]
+        en = pair["en"]
+        fr = pair["fr"]
+        en_ids = [1,*self.en_tokenizer.encode(en).ids,2] 
+        en_ids = torch.tensor((en_ids + [0] * max(0, self.context_len - len(en_ids)))[:self.context_len]).long()
+        fr_ids = self.fr_tokenizer.encode(fr).ids
+        fr_in = torch.tensor(([1, *fr_ids] + [0] * max(0, self.context_len - len(fr_ids) + 1))[:self.context_len]).long()
+        fr_out = torch.tensor(([*fr_ids, 2] + [0] * max(0, self.context_len - len(fr_ids) + 1))[:self.context_len]).long()
+        src_mask = (en_ids != 0).float().repeat(self.context_len, 1) 
+        trg_mask = (fr_in != 0).float().repeat(self.context_len, 1)
+        trg_mask = torch.tril(trg_mask, diagonal=0)
+        return {"en":en_ids, "fr_in":fr_in, "fr_out":fr_out, "src_mask":src_mask, "trg_mask": trg_mask}
+
+
+def collate_fn(data):
+    # <pad> = 0, <sos> = 1, and <eos> = 2
+    b_size = len(data)
+    src, trg_in, trg_out = [], [], []
+    src_max_len, trg_max_len = 0, 0
+
+    for i in range(b_size):
+        src_seq = [1,*data[i]["en"],2]
+        trg = data[i]["fr"]
+        trg_in_seq = [1, *trg]
+        trg_out_seq = [*trg, 2]
+        src += [src_seq]
+        trg_in += [trg_in_seq]
+        trg_out += [trg_out_seq]
+        src_max_len = max(src_max_len, len(src_seq))
+        trg_max_len = max(trg_max_len, len(trg_in_seq))
+
+    for i in range(b_size):
+        src[i] = src[i] + [0]*(src_max_len-len(src[i]))
+        len_trg = trg_max_len-len(trg_in[i])
+        trg_in[i] = trg_in[i] + [0]*(len_trg)
+        trg_out[i] = trg_out[i] + [0]*(len_trg)
+
+    en = torch.tensor(src, dtype=torch.long)    
+    fr_in = torch.tensor(trg_in, dtype=torch.long)
+    fr_out = torch.tensor(trg_out, dtype=torch.long)
+
+    # masks
+    src_mask = (en != 0).float().repeat(en.size(-1),1,1)\
+        .transpose(0,1)
+    trg_mask = (fr_in != 0).float()\
+        .repeat(fr_in.size(-1),1,1).transpose(0,1)
+    trg_mask = torch.tril(trg_mask)
+    trg_src_mask = (en != 0).float().repeat(fr_in.size(-1),1,1)\
+        .transpose(0,1)
+
+    return {
+        "src":en, 
+        "trg_in":fr_in,
+        "trg_out":fr_out,
+        "src_mask":src_mask,
+        "trg_mask":trg_mask,
+        "trg_src_mask":trg_src_mask
+    }
+
+        
+def create_dataloader(dataset, batch_size=64, pin_memory=True, num_workers=2):
+    shuffle = False
+    if dataset.split == "train": shuffle = True
+    dataloader = DataLoader(
+        dataset, batch_size=batch_size, shuffle=shuffle, pin_memory=pin_memory,
+          num_workers=num_workers
+    )
+    return dataloader
+
+        
+if __name__=="__main__":
+    splits = ["train", "validation", "test"]
+    for sp in splits:
+        print(f"Creating dataset for {sp}")
+        dataset = EnFrDataset(sp)
+        print(f"Creating dataloader for {sp}")
+        dataloader = create_dataloader(sp)
+        start_time = time.time()
+        for batch in dataloader:
+            src = batch["en"]
+        print(f"Processing took {time.time()-start_time:.2f} seconds for split {sp}")
+        
+            
+    
 
---
+++ transformer/model.py
@@ -0,0 +1,231 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import math
+
+# ======================================================================================
+
+# input embeddings for encoder and decoder
+class InputEmbedding(nn.Module):
+    """Calculates the embedding for input tokens
+    """
+    def __init__(self, vocab_size: int, d_model: int) -> None:
+        """Initialize the input embedding module with a given vocabulary size and model
+        dimension
+
+        Args:
+            vocab_size: size of the vocabulary. i.e number of tokens present in the 
+                vocabulary.
+            d_model: dimension to be used for the embedding and the subsequent
+                transformer model layers.
+        """
+        super(InputEmbedding, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, d_model)
+        self.vocab_size = vocab_size
+
+    def forward(self, x):
+        return self.embedding(x)
+
+
+# positional encoding for encoder and decoder
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=512):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, d_model)
+        positions = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div = torch.exp(-torch.arange(0, d_model, 2)/d_model * math.log(10000.0))
+        pe[:,0::2] = torch.sin(positions * div)
+        pe[:,1::2] = torch.cos(positions * div)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(1)]
+        return x
+
+
+# scaled dot-product attention
+class ScaledDotProductAttention(nn.Module):
+    def __init__(self):
+        super(ScaledDotProductAttention, self).__init__()
+
+    def forward(self, Q, K, V, attn_mask=None):
+        d_k = Q.size(-1)
+        scores = torch.matmul(Q, K.transpose(-2, -1)) / math.sqrt(d_k)
+        if attn_mask is not None:
+            try:
+                scores.masked_fill_(attn_mask==0, -1e9)
+            except:
+                breakpoint()
+        attn = F.softmax(scores, dim=-1)
+        self.attn = attn
+        context = torch.matmul(attn, V)
+        return context
+
+
+# Multi head attention
+class MultiHeadAttention(nn.Module):
+    def __init__(self, n_heads, d_model):
+        super(MultiHeadAttention, self).__init__()
+        self.d_model = d_model
+        self.n_heads = n_heads
+        self.W_Q = nn.Linear(d_model, d_model)
+        self.W_K = nn.Linear(d_model, d_model)
+        self.W_V = nn.Linear(d_model, d_model)
+        self.d_h = d_model // n_heads
+        self.W_O = nn.Linear(d_model, d_model)
+        self.sdpa = ScaledDotProductAttention()
+
+    def forward(self, Q, K, V, attn_mask):
+        B, _, C = Q.shape
+        assert C == self.d_model, f"Error: the dimension must be {self.d_model}"
+        Q = self.W_Q(Q).view(B, Q.size(1), self.n_heads, self.d_h).transpose(1,2)
+        K = self.W_K(K).view(B, K.size(1), self.n_heads, self.d_h).transpose(1,2)
+        V = self.W_V(V).view(B, V.size(1), self.n_heads, self.d_h).transpose(1,2)
+        context_heads = self.sdpa(Q, K, V, attn_mask.unsqueeze(1))
+        try:
+            context = context_heads.transpose(1,2).contiguous().view(B, Q.size(2), self.d_model)
+        except:
+            breakpoint()
+        return self.W_O(context)
+
+
+# Position-wise feed forward network
+class PositionWiseFeedForward(nn.Module):
+    def __init__(self, d_model):
+        super(PositionWiseFeedForward, self).__init__()
+        self.d_model = d_model
+        self.W1 = nn.Linear(d_model, 4 * d_model)
+        self.W2 = nn.Linear(4 * d_model, d_model)
+
+    def forward(self, x):
+        x = F.relu(self.W1(x))
+        return self.W2(x)
+
+
+# Encoder Block
+class EncoderBlock(nn.Module):
+    def __init__(self, d_model, n_heads, dropout):
+        super(EncoderBlock, self).__init__()
+        self.d_model = d_model
+        self.mha = MultiHeadAttention(n_heads, d_model)
+        self.pos_ffn = PositionWiseFeedForward(d_model)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout1d(dropout)
+
+    def forward(self, x, mask):
+        x = self.norm1(x + self.dropout(self.mha(x, x, x, mask)))
+        x = self.norm2(x + self.dropout(self.pos_ffn(x)))
+        return x
+
+
+# Encoder
+class Encoder(nn.Module):
+    def __init__(self, n_layers, d_model, n_heads, dropout):
+        super(Encoder, self).__init__()
+        self.n_layers = n_layers
+        self.d_model = d_model
+        self.blocks = nn.ModuleList(
+            [EncoderBlock(d_model, n_heads, dropout) for _ in range(n_layers)]
+        )
+        self.norm = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout1d(dropout)
+
+    def forward(self, x, mask)->torch.Tensor:
+        x = self.dropout(x)
+        for block in self.blocks:
+            x = block(x, mask)
+        return self.norm(x)
+
+
+# Decoder Block
+class DecoderBlock(nn.Module):
+    def __init__(self, d_model, n_heads, dropout):
+        super(DecoderBlock, self).__init__()
+        self.d_model = d_model
+        self.mha1 = MultiHeadAttention(n_heads, d_model)
+        self.mha2 = MultiHeadAttention(n_heads, d_model)
+        self.pos_ffn = PositionWiseFeedForward(d_model)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout1d(dropout)
+
+    def forward(self, x, x_enc, trg_mask, enc_mask):
+        x = self.norm1(x + self.dropout(self.mha1(x, x, x, trg_mask)))
+        x = self.norm2(x + self.dropout(self.mha2(x, x_enc, x_enc, enc_mask)))
+        x = self.norm3(x + self.dropout(self.pos_ffn(x)))
+        return x
+
+
+# Decoder
+class Decoder(nn.Module):
+    def __init__(self, d_model, n_heads, n_layers, dropout):
+        super(Decoder, self).__init__()
+        self.d_model = d_model
+        self.n_layers = n_layers
+        self.blocks = nn.ModuleList(
+            [DecoderBlock(d_model, n_heads, dropout) for _ in range(n_layers)]
+        )
+        self.norm = nn.LayerNorm(d_model)
+        self.dropout = nn.Dropout1d(dropout)
+
+    def forward(self, x, x_enc, trg_mask, enc_mask):
+        x = self.dropout(x)
+        for block in self.blocks:
+            x = block(x, x_enc, trg_mask, enc_mask)
+        return self.norm(x)
+
+
+# Transformer
+class Transformer(nn.Module):
+    def __init__(
+            self, 
+            encoder:Encoder, 
+            decoder:Decoder, 
+            enc_emb:InputEmbedding, 
+            dec_emb:InputEmbedding, 
+            enc_pos:PositionalEncoding, 
+            dec_pos:PositionalEncoding
+        ):
+        super(Transformer, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.enc_emb = enc_emb
+        self.dec_emb = dec_emb
+        self.enc_pos = enc_pos
+        self.dec_pos = dec_pos 
+        self.linear = nn.Linear(decoder.d_model, dec_emb.vocab_size)
+
+    def encode(self, x, mask):
+        x = self.enc_emb(x)
+        x = self.enc_pos(x)
+        x = self.encoder(x, mask)
+        return x
+
+    def decode(self, x, x_enc, trg_mask, enc_mask):
+        x = self.dec_emb(x)
+        x = self.dec_pos(x)
+        x = self.decoder(x, x_enc, trg_mask, enc_mask)
+        x = self.linear(x)
+        return x
+
+
+def build_transformer(
+        n_layers,
+        d_model,
+        n_heads,
+        dropout, 
+        src_vocab_size, 
+        trg_vocab_size
+    ):
+    return Transformer(
+        Encoder(n_layers, d_model, n_heads, dropout),
+        Decoder(d_model, n_heads, n_layers, dropout),
+        InputEmbedding(src_vocab_size, d_model),
+        InputEmbedding(trg_vocab_size, d_model),
+        PositionalEncoding(d_model),
+        PositionalEncoding(d_model)
+    )
+        
