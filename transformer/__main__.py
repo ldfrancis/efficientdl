a56966b1c3a396565af5bc4fcd223052fda1c77c
Lawrence Francis <lawrencedikeu@gmail.com>
2024-06-17 23:08:10 +0200

    Add tqdm for training steps

--- transformer/__main__.py
+++ transformer/__main__.py
@@ -8,6 +8,8 @@ from dataclasses import dataclass, asdict
 from dataset import EnFrDataset, create_dataloader
 import time
 
+from tqdm import tqdm
+
 @dataclass
 class ModelConfig:
     n_layers: int = 6
@@ -62,22 +64,25 @@ if __name__=="__main__":
     loss_fn = nn.CrossEntropyLoss(ignore_index=0)
     optimizer = optim.AdamW(model.parameters(), t_config.lr)
     
-    for batch in train_dataloader:
-        x_enc = model.encode(batch["en"].to(device), batch["src_mask"].to(device))
-        logits = model.decode(
-            batch["fr_in"].to(device),
-            x_enc, batch["trg_mask"].to(device),
-            batch["src_mask"].to(device)
-        )
-        steps += 1
-        optimizer.zero_grad()
-        loss = loss_fn(logits.view((-1, 30000)), batch["fr_out"].to(device).view((-1)))
-        loss.backward()
-        optimizer.step()
-        print(f"step: {steps} time: {(time.time()-t0)*1000:.2f}ms loss:{loss.item()}")
-        t0 = time.time()
-        if steps == 50:
-            break
+    epochs = 100
+    for epoch in tqdm(range(epochs)):
+        # one epoch training
+        for batch in train_dataloader:
+            x_enc = model.encode(batch["en"].to(device), batch["src_mask"].to(device))
+            logits = model.decode(
+                batch["fr_in"].to(device),
+                x_enc, batch["trg_mask"].to(device),
+                batch["src_mask"].to(device)
+            )
+            steps += 1
+            optimizer.zero_grad()
+            loss = loss_fn(logits.view((-1, 30000)), batch["fr_out"].to(device).view((-1)))
+            loss.backward()
+            optimizer.step()
+            tqdm.write(f"step: {steps} time: {(time.time()-t0)*1000:.2f}ms loss:{loss.item()}")
+            t0 = time.time()
+            if steps == 50:
+                break
 
 
 
